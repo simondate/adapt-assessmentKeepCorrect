c3c5820fd093e6373ef935f1079204991c8cf358
Simon Date <simondate@yahoo.co.uk>
2019-02-18 13:51:49 +0700

    Deleted console logs

--- js/adapt-asssessmentKeepCorrect.js
+++ js/adapt-asssessmentKeepCorrect.js
@@ -6,31 +6,24 @@ define(["coreJS/adapt"], function(Adapt) {
   var asssessmentKeepCorrect = Backbone.Model.extend({
 
     initialize: function() {
-      console.log(this);
       this.listenTo(Adapt, 'assessments:reset', this.reset);
       this.listenTo(Adapt, 'assessments:complete', this.complete);
       this.listenTo(Adapt, 'pageView:ready', this.pageReload);
     },
 
     pageReload: function() {
-      console.log('page reload');
       var context = this;
       if(!this.questions || !this.reset) return;
       this.reset = false;
-      console.log(this.assessmentModel);
       _.each(this.questions, function(question, index) {
-        console.log(index)
         if(question._isCorrect) {
           context.setCorrect(question._id);
-        } else {
-          console.log('question incorrect')
         }
       });
     },
 
     setCorrect: function(questionId) {
       var componentModel = Adapt.findById(questionId);
-      console.log(questionId);
       componentModel.set('_isCorrect', true);
       componentModel.set('_isSubmitted', true);
       componentModel.set('_isInteractionComplete', true);
@@ -41,13 +34,10 @@ define(["coreJS/adapt"], function(Adapt) {
       _.each(componentModel.get('_items'), function(item, index){
           var $itemLabel = $('.' + questionId).find('label').eq(index);
           var $itemInput = $('.' + questionId).find('input').eq(index);
-          console.log($itemLabel);
           $itemLabel.addClass('disabled');
           $itemInput.prop('disabled', true);
           if(item._shouldBeSelected) {
             $itemLabel.addClass('selected');
-
-            console.log($itemLabel);
           }
       });
     },
@@ -65,7 +55,7 @@ define(["coreJS/adapt"], function(Adapt) {
 
 
   Adapt.once("app:dataReady", function() {
-    //  if(!Adapt.course.get('_menuVideo') || !Adapt.course.get('_menuVideo')._isEnabled) return;
+    if(!Adapt.course.get('_assessmentKeepCorrect') || !Adapt.course.get('_assessmentKeepCorrect')._isEnabled) return;
     new asssessmentKeepCorrect();
   });
 
