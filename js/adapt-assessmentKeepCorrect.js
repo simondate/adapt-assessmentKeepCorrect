6694f4b3e17871730534ea629321e695de8210eb
Simon Date <simondate@yahoo.co.uk>
2019-02-24 15:22:10 +0700

    Removed reset code. When you return to page tick icon reapplied

--- js/adapt-assessmentKeepCorrect.js
+++ js/adapt-assessmentKeepCorrect.js
@@ -6,15 +6,13 @@ define(["coreJS/adapt"], function(Adapt) {
   var assessmentKeepCorrect = Backbone.Model.extend({
 
     initialize: function() {
-      this.listenTo(Adapt, 'assessments:reset', this.reset);
       this.listenTo(Adapt, 'assessments:complete', this.complete);
       this.listenTo(Adapt, 'pageView:ready', this.pageReload);
     },
 
     pageReload: function() {
       var context = this;
-      if (!this.questions || !this.reset) return;
-      this.reset = false;
+      if (!this.questions) return;
       _.each(this.questions, function(question, index) {
         if (question._isCorrect) {
           context.setCorrect(question._id);
@@ -32,7 +30,6 @@ define(["coreJS/adapt"], function(Adapt) {
       $component.find('label').addClass('disabled');
       $component.find('button').addClass('disabled');
       $component.find('label.selected').find('.mcq-correct-icon').show();
-      $component.find('.select2').addClass('disabled select2-container--disabled');
       if (componentModel.get('_component') === 'mcq') {
         _.each(componentModel.get('_items'), function(item, index) {
           var $itemLabel = $component.find('label').eq(index);
@@ -73,11 +70,6 @@ define(["coreJS/adapt"], function(Adapt) {
           });
         }
       });
-    },
-
-    reset: function(state, model) {
-      if (!this.questions) return;
-      this.reset = true;
     },
 
     complete: function(state, model) {
